ff03f4fb1dbc2098fb0fcef188e620b12d0ad684
fhuan10 <99567644+fhuan10@users.noreply.github.com>
2022-06-27 09:48:39 -0500

    Hello World

---
+++ week1/helloworld.js
@@ -0,0 +1,9 @@
+// Display the 'Hello World' message
+
+function hello() {
+    return "Hello World";
+}
+
+module.exports = hello;  // Export function
+
+// console.log(hello());  <== Useful for debugging/testing the code before committing changes
