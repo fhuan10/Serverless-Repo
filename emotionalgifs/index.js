0314e89762db78f4fde91cde66173d68bf5878ae
fhuan10 <99567644+fhuan10@users.noreply.github.com>
2022-07-17 18:35:38 -0500

    Emotional Gif Part 2

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,6 +1,7 @@
 // Declare a variable in order to access the NPM package 'parse-multipart'
 // Pretty much, bring code from the library into this folder, in which the code can be used here
 const multipart = require('parse-multipart');
+const fetch = require('node-fetch');  // Means it will import node-fetch from its code
  
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -14,12 +15,37 @@ module.exports = async function (context, req) {
     // parse the body
     const parts = multipart.Parse(body, boundary);
  
-    let convertedResult = Buffer.from(parts[0].data).toString('base64');
+    // let convertedResult = Buffer.from(parts[0].data).toString('base64');
     // FILL IN THE BLANK
  
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
-    };
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: convertedResult
+    // };
+
+    // 'async function' means that this function will be running in the background
+    async function analyzeImage(img){
+        const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+        const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+        // This is creating URL search (after the website.com/______ <---)
+        let params = new URLSearchParams({
+            'returnFaceId': 'true',
+            '<PARAMETER NAME>': 'emotion'
+        })
+
+        let resp = await fetch(uriBase + '?' + params.toString(), {
+            method: 'POST',  //WHAT TYPE OF REQUEST?
+            body: img,  //WHAT ARE WE SENDING TO THE API?
+            headers: {
+                'Content-Type': 'application/octet-stream',
+                "Ocp-Apim-Subscription-Key": subscriptionKey  //do this in the next section
+            }
+        })
+        let data = await resp.json();  // await always in asynch
+        
+        return data;
+    }
 }
+
 
